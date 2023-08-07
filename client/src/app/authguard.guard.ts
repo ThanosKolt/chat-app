4103827c8fcc7e772475362e48719949d0f3a9c0
Thanos Koltsidas <tkoltsidas@SSPC.GR>
2023-08-07 12:46:50 +0300

    fix: fixed some redirect bugs

--- client/src/app/authguard.guard.ts
+++ client/src/app/authguard.guard.ts
@@ -3,6 +3,15 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 export const authguardGuard: CanActivateFn = (route, state) => {
+  const username = localStorage.getItem('currentUserUsername');
+  const id = Number(localStorage.getItem('currentUserId'));
+  if (username && id && localStorage.getItem('token')) {
+    let authService = inject(AuthService);
+    authService.isLoggedIn.next(true);
+    authService.currentUser.next({ id, username });
+
+    return true;
+  }
   let isLoggedIn = inject(AuthService).isLoggedIn.getValue();
   if (!isLoggedIn) {
     inject(Router).navigate(['login']);
