61ee345077295ce006e337cd82d8c3289b7fcff0
Thanos Koltsidas <thanoskoltsidas@gmail.com>
2023-08-12 17:53:27 +0300

    (feat): error handler
    - added express-async-errors package
    - created custom errors
    - created a custom error handler
    Now instead of using try/catch block and having to write the same response every time you just throw the custom errors

--- server/src/controllers/chat.ts
+++ server/src/controllers/chat.ts
@@ -4,51 +4,40 @@ import { User } from "../entity/User";
 import { Room } from "../entity/Room";
 import { StatusCodes } from "http-status-codes";
 import { In, Not } from "typeorm";
+import { BadRequestError, NotFoundError } from "../errors/errors";
 
 const manager = AppDataSource.manager;
 
 const roomRepository = AppDataSource.getRepository(Room);
-const userRepository = AppDataSource.getRepository(User);
 
 export const createRoom = async (req: Request, res: Response) => {
   const userAId: number = Number(req.body.userAId);
   const userBId: number = Number(req.body.userBId);
 
   if (!userAId || !userBId) {
-    res
-      .status(StatusCodes.BAD_REQUEST)
-      .json({ error: { msg: "You need to provide two user ids" } });
-    return;
+    throw new BadRequestError("You need to provide two user ids");
   }
 
   const isSelf = userAId === userBId;
 
-  try {
-    const doesRoomExist = await getRoom(userAId, userBId, isSelf);
+  const doesRoomExist = await getRoom(userAId, userBId, isSelf);
 
-    if (doesRoomExist !== null) {
-      res.status(StatusCodes.BAD_REQUEST).json({
-        error: { msg: `This room already exists with id: ${doesRoomExist.id}` },
-      });
-      return;
-    }
-
-    const userA = await manager.findOneBy(User, { id: userAId });
-    const userB = await manager.findOneBy(User, { id: userBId });
-
-    const newRoom = new Room();
-    if (userA !== null && userB !== null) {
-      newRoom.isSelf = isSelf;
-      newRoom.users = [userA, userB];
-      await manager.save(newRoom);
-    }
-    res.status(StatusCodes.CREATED).json({ roomId: newRoom.id });
-  } catch (error) {
-    console.error(error.message);
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ error: { msg: "something went wrong." } });
+  if (doesRoomExist !== null) {
+    throw new BadRequestError(
+      `This room already exists with id: ${doesRoomExist.id}`
+    );
   }
+
+  const userA = await manager.findOneBy(User, { id: userAId });
+  const userB = await manager.findOneBy(User, { id: userBId });
+
+  const newRoom = new Room();
+  if (userA !== null && userB !== null) {
+    newRoom.isSelf = isSelf;
+    newRoom.users = [userA, userB];
+    await manager.save(newRoom);
+  }
+  res.status(StatusCodes.CREATED).json({ roomId: newRoom.id });
 };
 
 export const getRoomId = async (req: Request, res: Response) => {
@@ -62,80 +51,51 @@ export const getRoomId = async (req: Request, res: Response) => {
       .json({ error: { msg: "You need to provide two user ids" } });
     return;
   }
-  try {
-    const room = await getRoom(userAId, userBId, isSelf);
-    if (!room) {
-      res.status(StatusCodes.NOT_FOUND).json({
-        error: {
-          msg: `Didnt find a chat room for users: ${userAId} ${userBId}`,
-        },
-      });
-      return;
-    }
-    res.status(StatusCodes.OK).json({ roomId: room.id, users: room.users });
-  } catch (error) {
-    res
-      .status(StatusCodes.BAD_REQUEST)
-      .json({ error: { msg: "Something wen't wrong, please try again." } });
+  const room = await getRoom(userAId, userBId, isSelf);
+  if (room === null) {
+    throw new NotFoundError(
+      `Didnt find a chat room for users: ${userAId} ${userBId}`
+    );
   }
+  res.status(StatusCodes.OK).json({ roomId: room.id, users: room.users });
 };
 
 export const getRoomsByUser = async (req: Request, res: Response) => {
   const userId = Number(req.body.userId);
-  try {
-    const rooms = await roomRepository.find({
-      where: { users: { id: userId } },
-    });
-    const roomsIds = rooms.map((room) => room.id);
-    const roomsWithUsers = await roomRepository.find({
-      where: { id: In(roomsIds), users: { id: Not(userId) } },
-      relations: { users: true },
-    });
-    const result = roomsWithUsers.map((room) => {
-      return {
-        roomId: room.id,
-        user: { id: room.users[0].id, username: room.users[0].username },
-      };
-    });
-    res.status(StatusCodes.OK).json(result);
-  } catch (error) {
-    console.error(error.message);
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ error: { msg: "Something wen't wrong" } });
-  }
+  const rooms = await roomRepository.find({
+    where: { users: { id: userId } },
+  });
+  const roomsIds = rooms.map((room) => room.id);
+  const roomsWithUsers = await roomRepository.find({
+    where: { id: In(roomsIds), users: { id: Not(userId) } },
+    relations: { users: true },
+  });
+  const result = roomsWithUsers.map((room) => {
+    return {
+      roomId: room.id,
+      user: { id: room.users[0].id, username: room.users[0].username },
+    };
+  });
+  res.status(StatusCodes.OK).json(result);
 };
 
 export const getRoomInfo = async (req: Request, res: Response) => {
   const roomId = Number(req.body.roomId);
   if (!roomId) {
-    res
-      .status(StatusCodes.BAD_REQUEST)
-      .json({ error: { msg: `You need to provide a roomId` } });
-    return;
+    throw new BadRequestError("You need to provide a roomId");
   }
-  try {
-    const room = await roomRepository.findOne({
-      relations: { users: true },
-      where: { id: roomId },
-    });
-    if (room !== null) {
-      const userResult = room.users.map((user) => {
-        return { id: user.id, username: user.username };
-      });
-      res.status(StatusCodes.OK).json({ roomId: room.id, users: userResult });
-      return;
-    }
-    res
-      .status(StatusCodes.NOT_FOUND)
-      .json({ msg: `Room with id: ${roomId} not found` });
-    return;
-  } catch (error) {
-    console.error(error.message);
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ error: { msg: "Something went wrong" } });
+  const room = await roomRepository.findOne({
+    relations: { users: true },
+    where: { id: roomId },
+  });
+  if (room === null) {
+    throw new NotFoundError(`Room with id: ${roomId} not found`);
   }
+  const userResult = room.users.map((user) => {
+    return { id: user.id, username: user.username };
+  });
+  res.status(StatusCodes.OK).json({ roomId: room.id, users: userResult });
+  return;
 };
 
 const getRoom = async (userAId: number, userBId: number, isSelf: boolean) => {
---
+++ server/src/middleware/errorHandler.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from "express";
+import { CustomError } from "../errors/errors";
+
+export function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (err instanceof CustomError) {
+    return res.status(err.status).json({ error: { message: err.message } });
+  }
+  return res.status(500).json({ error: { message: "something went worng" } });
+}
