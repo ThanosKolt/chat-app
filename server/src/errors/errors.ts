ba2cb77771051797037f2bd030a2cca1d1e952ff
Thanos Koltsidas <thanoskoltsidas@gmail.com>
2023-08-12 21:01:44 +0300

    (fix): always throwing default error
    if block of errorHandler never ran
    for instanceof to work i wrote Object.setPrototypeOf in the error classes

--- server/src/errors/errors.ts
+++ server/src/errors/errors.ts
@@ -13,17 +13,20 @@ export class CustomError extends Error implements ICustomError {
 export class NotFoundError extends CustomError {
   constructor(message: string) {
     super(StatusCodes.NOT_FOUND, message);
+    Object.setPrototypeOf(this, CustomError.prototype);
   }
 }
 
 export class BadRequestError extends CustomError {
   constructor(message: string) {
     super(StatusCodes.BAD_REQUEST, message);
+    Object.setPrototypeOf(this, CustomError.prototype);
   }
 }
 
 export class UnauthorizedError extends CustomError {
   constructor(message: string) {
     super(StatusCodes.UNAUTHORIZED, message);
+    Object.setPrototypeOf(this, CustomError.prototype);
   }
 }
